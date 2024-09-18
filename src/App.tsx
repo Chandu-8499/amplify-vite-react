da54fbd468e22946809fa59153872d0fa2c1ceac
Chandu-8499 <gana.chandu999@gmail.com>
2024-09-19 01:08:37 +0530

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Authenticator } from '@aws-amplify/ui-react';
 import AdminDashboard from './AdminDashboard';
+import Login from './pages/Login';  
+import Signup from './pages/Signup'; 
 
 function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <AdminDashboard 
-          user={user} // Passing user object
-          signOut={signOut} // Passing signOut function
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route 
+          path="/admin" 
+          element={
+            <Authenticator>
+              {({ signOut, user }) => (
+                <AdminDashboard user={user} signOut={signOut} />
+              )}
+            </Authenticator>
+          } 
         />
-      )}
-    </Authenticator>
+        <Route path="*" element={<Navigate to="/login" />} /> 
+      </Routes>
+    </Router>
   );
 }
 
