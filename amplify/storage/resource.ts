88173e8c2420596847677c1b4038f3db4159b2bd
Chandu-8499 <gana.chandu999@gmail.com>
2024-09-30 14:27:11 +0530

    product page and storage

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,15 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+    name: 'Ecommerce application storage',
+    access: (allow) => ({
+      'product-pictures/{entity_id}/*': [
+        allow.guest.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+      'picture-submissions/*': [
+        allow.authenticated.to(['read','write']),
+        allow.guest.to(['read', 'write'])
+      ],
+    })
+  });
