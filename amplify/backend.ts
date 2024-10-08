e491ce404c38466c38b33eef79ebf9eb1cbf1062
Chandu-8499 <gana.chandu999@gmail.com>
2024-10-08 10:07:35 +0530

    backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,12 +1,13 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-// import { data } from './data/resource';
-// import { storage } from './storage/resource';
+import { data } from './data/resource';
+import { storage } from './storage/resource';
 // import { addUserToDB } from './functions/add-user/resource';
 
 defineBackend({
   auth,
-  
+  data,
+  storage
 });
 
 // defineBackend({ 
