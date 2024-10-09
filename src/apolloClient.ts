0dbbb924cdb522a28fdb6cffc79a5c17d9cdb42a
Chandu-8499 <gana.chandu999@gmail.com>
2024-10-09 10:30:26 +0530

    client

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,22 +1,20 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { createAuthLink } from 'aws-appsync-auth-link';
-
-// Use the AppSync details you retrieved
-const GRAPHQL_API_URL = 'https://cjiaohomsbbmvf5h6opbvp5dra.appsync-api.ap-south-1.amazonaws.com/graphql';
-const API_KEY = 'da2-f4ay6e4ztujcfjdqwlpxjgtm3oq';
-const AWS_REGION = 'ap-south-1';
-
+import outputs from "../amplify_outputs.json";
+// Rest of the code remains the same
+// Create the Apollo Client configuration
 const createApolloClient = () => {
+  
   const httpLink = createHttpLink({
-    uri: GRAPHQL_API_URL,
+    uri: outputs.data.url,
   });
 
   const authLink = createAuthLink({
-    url: GRAPHQL_API_URL,
-    region: AWS_REGION,
+    url: outputs.data.url,
+    region: outputs.data.aws_region,
     auth: {
       type: 'API_KEY',
-      apiKey: API_KEY,
+      apiKey: outputs.data.api_key,
     },
   });
 
@@ -26,4 +24,4 @@ const createApolloClient = () => {
   });
 };
 
-export const client = createApolloClient();
+export const client = createApolloClient()
