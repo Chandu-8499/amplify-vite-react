ce9d1fe3fae9600d010787782df19d56cde6a1ae
Chandu-8499 <gana.chandu999@gmail.com>
2024-10-09 09:31:59 +0530

    apolloclient

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -2,8 +2,8 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { createAuthLink } from 'aws-appsync-auth-link';
 
 // Use the AppSync details you retrieved
-const GRAPHQL_API_URL = 'https://djnzkeo2mbchfnc3jiyk6ppana.appsync-api.ap-south-1.amazonaws.com/graphql';
-const API_KEY = 'da2-afcstriqwra4zfhzrypv75zbt4';
+const GRAPHQL_API_URL = 'https://cjiaohomsbbmvf5h6opbvp5dra.appsync-api.ap-south-1.amazonaws.com/graphql';
+const API_KEY = 'da2-f4ay6e4ztujcfjdqwlpxjgtm3oq';
 const AWS_REGION = 'ap-south-1';
 
 const createApolloClient = () => {
